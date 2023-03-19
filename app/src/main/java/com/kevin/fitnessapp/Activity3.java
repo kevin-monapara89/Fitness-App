5073cf9778a72e4a1c79fa7931ca14ffd687b7c2
kevin monapara <kevinmonapara89@gmail.com>
2023-03-19 16:02:37 +0530

    commit

--- app/src/main/java/com/kevin/fitnessapp/Activity3.java
+++ app/src/main/java/com/kevin/fitnessapp/Activity3.java
@@ -7,23 +7,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Activity3 extends AppCompatActivity {
 
     ImageView btnback1;
+    TextView btnlog3;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
         btnback1 = findViewById(R.id.btnback1);
+        btnlog3 = findViewById(R.id.btnlog3);
 
         btnback1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(Activity3.this, Activity2.class);
+                startActivity(in);
+            }
+        });
+        btnlog3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Activity3.this, Activity4.class);
                 startActivity(in);
             }
         });
--- app/src/main/java/com/kevin/fitnessapp/Activity4.java
+++ app/src/main/java/com/kevin/fitnessapp/Activity4.java
@@ -2,13 +2,39 @@ package com.kevin.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Activity4 extends AppCompatActivity {
 
+    TextView btnlog4;
+    ImageView btnback2;
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4);
+
+        btnback2 = findViewById(R.id.btnback2);
+        btnlog4 = findViewById(R.id.btnlog4);
+
+        btnback2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Activity4.this, Activity3.class);
+                startActivity(in);
+            }
+        });
+        btnlog4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Activity4.this, Activity5.class);
+                startActivity(in);
+            }
+        });
     }
 }
