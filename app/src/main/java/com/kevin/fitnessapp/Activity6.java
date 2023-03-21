edd483c7432ae1b45c2c4618beeb2723bc14a10f
kevin monapara <kevinmonapara89@gmail.com>
2023-03-21 21:05:19 +0530

    commit .

--- app/src/main/java/com/kevin/fitnessapp/Activity6.java
+++ app/src/main/java/com/kevin/fitnessapp/Activity6.java
@@ -2,13 +2,26 @@ package com.kevin.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class Activity6 extends AppCompatActivity {
-
+    RelativeLayout connectdevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6);
+
+        connectdevice = findViewById(R.id.connectdevice);
+
+        connectdevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Activity6.this, Activity7.class);
+                startActivity(in);
+            }
+        });
     }
 }
