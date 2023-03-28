ca2ad35fd70e95f0f436d131e1d136d62898d4ee
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-03-28 00:01:52 -0400

    commit

---
+++ app/src/main/java/com/kevin/fitnessapp/Activity13.java
@@ -0,0 +1,14 @@
+package com.kevin.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_13);
+    }
+}
